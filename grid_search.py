d9ad4db4c6378cb31beeb96904839acbd0f1687b
Alexander Kim <alex000kim@gmail.com>
2022-06-27 12:17:43 -0400

    dvc pipeline based on papermill

--- grid_search.py
+++ grid_search.py
@@ -6,9 +6,8 @@ import subprocess
 n_estimators_values = range(50, 250, 50)
 max_depth_values = range(10, 25, 5)
 
-# Iterate over all combinations of hyperparameter values.
+# Iterate over all combinations of hyperparameter values
 for n_estimators, max_depth in itertools.product(n_estimators_values, max_depth_values):
-    # Execute "dvc exp run --queue --set-param train.n_est=<n_est> --set-param train.min_split=<min_split>".
     subprocess.run(["dvc", "exp", "run", "--queue",
                     "--set-param", f"train.params.n_estimators={n_estimators}",
                     "--set-param", f"train.params.max_depth={max_depth}"])
