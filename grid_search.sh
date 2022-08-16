5d212445675f562da08b2abc4aec3fc8aa7fc6b1
Alexander Kim <alex000kim@gmail.com>
2022-08-16 13:04:46 -0400

    update grid_search.sh

--- grid_search.sh
+++ grid_search.sh
@@ -1,6 +1,7 @@
+#!/bin/sh
 declare -a n_estimators_values=(50 100 150)
 declare -a max_depth_values=(10 15 20)
-## now loop through the above array
+
 for n_est in "${n_estimators_values[@]}"
     do
         for max_d in "${max_depth_values[@]}"
