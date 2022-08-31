795c94965c69ef299694d681b8489b7f784195d9
Alexander Kim <alex000kim@gmail.com>
2022-08-31 10:29:43 -0400

    update

---
+++ grid_search.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+declare -a n_estimators_values=(50 100 150)
+declare -a max_depth_values=(10 15 20)
+
+for n_est in "${n_estimators_values[@]}"
+    do
+        for max_d in "${max_depth_values[@]}"
+            do
+            dvc exp run -S train.params.n_estimators=$n_est -S train.params.max_depth=$max_d
+            done
+    done
