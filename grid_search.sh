e7e04a707807061515403a0803aec76dc0b83e78
Alexander Kim <alex000kim@gmail.com>
2022-09-26 15:42:37 -0400

    dvc-hydra hyperparam tuning

--- grid_search.sh
+++ grid_search.sh
@@ -1,11 +1,4 @@
 #!/bin/sh
-declare -a n_estimators_values=(50 100 150)
-declare -a max_depth_values=(10 15 20)
-
-for n_est in "${n_estimators_values[@]}"
-    do
-        for max_d in "${max_depth_values[@]}"
-            do
-            dvc exp run -S train.params.n_estimators=$n_est -S train.params.max_depth=$max_d
-            done
-    done
+dvc exp run -S 'train.params.n_estimators=range(50, 200, 30)' \
+-S 'train.params.max_depth=range(10, 25, 5)' --queue
+dvc exp run --run-all
